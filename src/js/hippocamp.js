6daa24a00b262c081882b93c076bc9129556b8e9
sangretu <github@geeq.com>
2020-05-11 18:20:24 -0700

    first ui element

--- src/js/hippocamp.js
+++ src/js/hippocamp.js
@@ -9,7 +9,58 @@
 
 }
 
+
+{ /* Globals */
+  var foo   = null; // test object
+  
+  var content = null;//
+}
+
 { /* Classes */
 
 
+}
+
+{ /* Modes */
+
+  { // CVC
+  
+    var makeButton = function(params)
+    {
+      var newButton = document.createElement('button');
+      newButton.classList.add('btnCVC');
+      newButton.textContent = 'undefined';
+      return newButton;
+    };
+    
+    var placeButtons = function(params)
+    {
+      content.appendChild(makeButton());
+    }
+    
+  }
+
+}
+
+{ /* Main */
+
+  function main()
+  {
+    console.log('foo');
+    init();
+    placeButtons();
+  }
+  
+  function init()
+  {
+    // init globals
+    content = document.getElementById('content');
+  };
+
+  // Avoiding jQuery dependency, but I'm not sure how reliable this is.
+  document.addEventListener("DOMContentLoaded", function(event)
+  { 
+    main();
+  });
+  
 }
